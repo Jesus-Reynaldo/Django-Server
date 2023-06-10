ec8594c3efd2ce355f871b9e86bc1ec94bbcddd1
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-06-10 03:41:42 -0400

    actualizando

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+'''
 from dotenv import load_dotenv
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
-
+'''
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
